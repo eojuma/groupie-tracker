95e883cf5fde610c940c06acf7cc1e9f1d6155aa
ibrsamwel <ibraah568@gmail.com>
2026-03-11 10:48:11 +0000

    Implement GetArtists function to fetch artists from API

--- backend/services.go
+++ backend/services.go
@@ -1,1 +1,25 @@
 package backend
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// GetArtists fetches all artists from the API
+func GetArtists() ([]Artist, error) {
+	url := "https://groupietrackers.herokuapp.com/api/artists"
+
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	var artists []Artist
+	err = json.NewDecoder(resp.Body).Decode(&artists)
+	if err != nil {
+		return nil, err
+	}
+
+	return artists, nil
+}
