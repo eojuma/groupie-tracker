de37349984f64f71bb0aa5c7994acb9de5dbe782
ibrsamwel <ibraah568@gmail.com>
2026-03-11 10:37:21 +0000

    Define Artist struct in models.go

--- backend/models.go
+++ backend/models.go
@@ -1,1 +1,10 @@
 package backend
+
+type Artist struct {
+	ID           int      `json:"id"`
+	Image        string   `json:"image"`
+	Name         string   `json:"name"`
+	Members      []string `json:"members"`
+	CreationDate int      `json:"creationDate"`
+	FirstAlbum   string   `json:"firstAlbum"`
+}
